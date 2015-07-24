5c6242317b363e508fbfd89a3a97a253d4763903
Erick MM <erick.morfin+localgit@gmail.com>
2015-07-24 17:41:12 -0500

    implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## This module implements matrix inversion with caching.
 
-## Write a short comment describing this function
+## makeCacheMatrix(x) creates a cached matrix object from a standard matrix x.
+##
+## Arguments:
+## x        A square, nonsingular matrix.
+##
+## Details:
+## The supplied matrix x should be invertible, although this is not validated. 
+## If x is not supplied, it defaults to an empty matrix.
+## The returned object is a special "cached matrix" object which wraps 
+## the supplied matrix x, a cache for storing the inverse of x, and four access 
+## methods.
+## The matrix can be retrieved with the get() method and updated with
+## the set() method. Setting the matrix has the secondary effect of clearing 
+## (setting to NULL) the cache.
+## The cache is initialized to NULL and can be accessed via the setInverse() 
+## and getInverse() methods.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mInverse <- NULL
+        set <- function(y) {
+                x <<- y
+                mInverse <<- NULL
+        }
+        get <- function() { 
+                return(x) 
+        }
+        setInverse <- function(i) {
+                mInverse <<- i
+        }
+        getInverse <- function() {
+                return(mInverse)
+        }
+        list(set = set, 
+             get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x) returns the inverse of the matrix stored in x.
+##
+## Arguments:
+## x        A "cached matrix" object created with the makeCacheMatrix() 
+##          function.
+## ...      Not used in the current implementation.
+##
+## Details:
+## Returns a standard matrix object which corresponds to the inverse of the 
+## matrix stored in x.
+## This function first tries to retrieve the inverse from x's cache. If the 
+## cache is empty (equal to NULL), the inverse matrix is computed, stored in 
+## x's cache, and returned. Otherwise, the cached valued is returned.
+## The matrix is assumed to be invertible and no checks are performed before 
+## invoking R's solver.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if (is.null(i)) {
+                i <- solve(x$get())
+                x$setInverse(i)
+        }
+        return(i)
 }
